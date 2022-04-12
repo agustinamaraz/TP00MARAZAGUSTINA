4d0fafc2d973b366b5705ea463d25fd4bffcb6d3
Alumno <Alumno@192.168.0.9>
2022-04-11 23:48:51 -0300

    Actualizacion del patron de diseño de MVC

--- src/main/java/ar/edu/unju/edm/controller/CalculadoraController.java
+++ src/main/java/ar/edu/unju/edm/controller/CalculadoraController.java
@@ -9,28 +9,127 @@ import ar.edu.unju.edm.model.Calculadora;
 
 @Controller
 public class CalculadoraController {
-	
+
 	@GetMapping("/menu")
-	
+
 	public String mostrarMenu() {
-		
+
 		return ("Index");
 	}
-	
+
 	@GetMapping("/resolverSuma")
-	
-	public String getSumaPage(@RequestParam (name = "n1") int n1 , @RequestParam (name = "n2") int n2 , Model model) {
-		
+
+	public String getSumaPage(@RequestParam(name = "n1") int n1, @RequestParam(name = "n2") int n2, Model model) {
+
 		Calculadora nuevaCalculadora = new Calculadora();
-		
+
 		nuevaCalculadora.setN1(n1);
 		nuevaCalculadora.setN2(n2);
-		
+
 		int resultadoS = 0;
 		resultadoS = nuevaCalculadora.resolverSuma();
-		
+
 		model.addAttribute("resultadoS", resultadoS);
+
+		return "resultadoSuma";
+	}
+
+	@GetMapping("/resolverResta")
+
+	public String getRestaPage(@RequestParam(name = "n1") int n1, @RequestParam(name = "n2") int n2, Model model) {
+
+		Calculadora nuevaCalculadora = new Calculadora();
+
+		nuevaCalculadora.setN1(n1);
+		nuevaCalculadora.setN2(n2);
+
+		int resultadoR = 0;
+		resultadoR = nuevaCalculadora.resolverResta();
+
+		model.addAttribute("resultadoR", resultadoR);
+
+		return "resultadoResta";
+	}
+	
+	@GetMapping("/resolverMultiplicacion")
+
+	public String getMultiplicacionPage(@RequestParam(name = "n1") int n1, @RequestParam(name = "n2") int n2, Model model) {
+
+		Calculadora nuevaCalculadora = new Calculadora();
+
+		nuevaCalculadora.setN1(n1);
+		nuevaCalculadora.setN2(n2);
+
+		int resultadoM = 0;
+		resultadoM = nuevaCalculadora.resolverMultiplicacion();
+
+		model.addAttribute("resultadoM", resultadoM);
+
+		return "resultadoMultiplicacion";
+	}
+	
+	@GetMapping("/resolverDivision")
+
+	public String getDivisionPage(@RequestParam(name = "n1") int n1, @RequestParam(name = "n2") int n2, Model model) {
+
+		Calculadora nuevaCalculadora = new Calculadora();
+
+		nuevaCalculadora.setN1(n1);
+		nuevaCalculadora.setN2(n2);
+
+		float resultadoD = 0;
 		
-		return "resultado";
+		if(n2!=0) {
+			resultadoD = nuevaCalculadora.resolverDivision();
+			model.addAttribute("resultadoD", resultadoD);
+			return "resultadoDivision";
+		}else {
+			return "resultadoDivisionIncorrecta";
+		}
+		
+
+		
+	}
+	
+	@GetMapping("/resolverPotencia")
+
+	public String getPotenciaPage(@RequestParam(name = "n1") int n1, @RequestParam(name = "n2") int n2, Model model) {
+
+		Calculadora nuevaCalculadora = new Calculadora();
+
+		nuevaCalculadora.setN1(n1);
+		nuevaCalculadora.setN2(n2);
+
+		int resultadoP = 0;
+		resultadoP = nuevaCalculadora.resolverPotencia();
+
+		model.addAttribute("resultadoP", resultadoP);
+
+		return "resultadoPotencia";
+	}
+	
+	@GetMapping("/resolverRaiz")
+
+	public String getRaizPage(@RequestParam(name = "n1") int n1, @RequestParam(name = "n2") int n2, Model model) {
+
+		Calculadora nuevaCalculadora = new Calculadora();
+
+		nuevaCalculadora.setN1(n1);
+		nuevaCalculadora.setN2(n2);
+
+		double resultadoRA = 0;
+		
+		if(n2!=0) {
+			resultadoRA = nuevaCalculadora.resolverRaiz();
+
+			model.addAttribute("resultadoRA", resultadoRA);
+			
+			return "resultadoRaiz";
+		}else {
+			return "resultadoRaizIncorrecta";
+		}
+		
+
+		
 	}
 }
